4c8068301baafcc5772c91d675ed538636e2fc14
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-17 16:33:38 -0300

    Update ProductoList.js

--- src/features/producto/ProductoList.js
+++ src/features/producto/ProductoList.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Producto from "./Producto";
 import axios from 'axios'
 const ProductoList = () => {
-    const [productos,setProductos] = [
+    const [productos,setProductos] = useState([
         {"id":"1", "nombre":"Bebida Cola", "precio":"1200", "imagen": "1.jpg"},
         {"id":"2", "nombre":"Bebida Fanta", "precio":"2100", "imagen": "2.jpg"},
         {"id":"3", "nombre":"Jugo Naranja", "precio":"2200", "imagen": "3.jpg"},
         {"id":"4", "nombre":"Pack Lata", "precio":"3420", "imagen": "4.jpg"},
-    ]
-    useEffect(async()=>{
-          await axios.get('https://g9cd7530b8a8613-db20220601151235.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/producto/')
-          .then(datos => setProductos(datos.items))
+    ])
+    useEffect(()=>{
+          axios.get('https://g9cd7530b8a8613-db20220601151235.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/producto/')
+          .then(data => setProductos(data.data.items))
         },[]
         )
     const modificaProducto = () => {
@@ -19,7 +19,7 @@ const ProductoList = () => {
     return(
         <div>
         {productos.map((producto)=>{
-            return(<Producto data={producto} key={producto.id} unbotonClick={modificaProducto} />)
+            return(<Producto data={producto} key={producto.key} unbotonClick={modificaProducto} />)
             
         })}
         </div>
