b3c450955b1747509b26a5f87a4db4d3f1c64577
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-17 16:25:05 -0300

    Update ProductoList.js

--- src/features/producto/ProductoList.js
+++ src/features/producto/ProductoList.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Producto from "./Producto";
-
+import axios from 'axios'
 const ProductoList = () => {
-    const productos = [
+    const [productos,setProductos] = [
         {"id":"1", "nombre":"Bebida Cola", "precio":"1200", "imagen": "1.jpg"},
         {"id":"2", "nombre":"Bebida Fanta", "precio":"2100", "imagen": "2.jpg"},
         {"id":"3", "nombre":"Jugo Naranja", "precio":"2200", "imagen": "3.jpg"},
         {"id":"4", "nombre":"Pack Lata", "precio":"3420", "imagen": "4.jpg"},
     ]
+    useEffect(async()=>{
+          await axios.get('https://g9cd7530b8a8613-db20220601151235.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/producto/')
+          .then(datos => setProductos(datos.items))
+        },[]
+        )
     const modificaProducto = () => {
         console.log(productos)
     }
